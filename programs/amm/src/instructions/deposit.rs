74e5fda31ff303526f024ff7f2f9120f8c2cfa72
ozoneRatchapon <61614162+ozoneRatchapon@users.noreply.github.com>
2025-02-09 10:39:49 +0700

    Implement AMM error handling and add deposit, withdraw, and swap instructions

--- programs/amm/src/instructions/deposit.rs
+++ programs/amm/src/instructions/deposit.rs
@@ -9,8 +9,146 @@ use crate::{errors::AmmError, state::Config};
 
 #[derive(Accounts)]
 pub struct Deposit<'info> {
+    // lp
     #[account(mut)]
-    pub user: Signer<'info>,
+    pub lp_provider: Signer<'info>,
     pub mint_x: Account<'info, Mint>,
     pub mint_y: Account<'info, Mint>,
+    #[account(
+        has_one = mint_x,
+        has_one = mint_y,
+        seeds = [b"config",config.seed.to_le_bytes().as_ref()],
+        bump = config.config_bump,
+    )]
+    pub config: Account<'info, Config>,
+
+    #[account(
+        seeds = [b"lp".config.key.as_ref()],
+        bump = config.lp_bump,
+        mint::decimals = 6,
+        mint::authority = config,
+    )]
+    pub mint_lp: Account<'info, Mint>,
+
+    // AMM ATAs
+    #[account(
+        mut,
+        associated_token::mint = mint_x,
+        associated_token::authority = config,
+    )]
+    pub vault_x: Account<'info, TokenAccount>,
+
+    #[account(
+        mut,
+        associated_token::mint = mint_y,
+        associated_token::authority = config,
+    )]
+    pub vault_y: Account<'info, TokenAccount>,
+
+    // ATAs
+    #[account(
+        mut,
+        associated_token::mint = mint_x,
+        associated_token::authority = lp_provider,
+    )]
+    pub lp_provider_mint_x_ata: Account<'info, TokenAccount>,
+
+    #[account(
+        mut,
+        associated_token::mint = mint_y,
+        associated_token::authority = lp_provider,
+    )]
+    pub lp_provider_mint_y_ata: Account<'info, TokenAccount>,
+
+    #[account(
+        init_if_needed,
+        payer = lp_provider,
+        associated_token::mint = mint_lp,
+        associated_token::authority = lp_provider,
+    )]
+    pub lp_provider_mint_lp_ata: Account<'info, TokenAccount>,
+
+    pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub system_program: Program<'info, System>,
+}
+
+impl<'info> Deposit<'info> {
+    pub fn deposit(
+        &mut self,
+        lp_amount: u64, // amount of LP tokens that the user wants to claim
+        max_x: u64,     // Maximum amount of x tokens that user is willing to deposit
+        max_y: u64,     // Maximum amount of y tokens that user is willing to deposit
+    ) -> Result<()> {
+        // LP = TRUMP and MELANIA coins as deposited
+
+        let (x, y) = match self.mint_lp.supply == 0
+            && self.vault_x.amount == 0
+            && self.vault_y.amount == 0
+        {
+            true => (max_x, max_y),
+            false => {
+                let amounts = ConstantProduct::xy_deposit_amounts_from_l(
+                    self.vault_x.amount,
+                    self.vault_y.amount,
+                    self.mint_lp.supply,
+                    lp_amount,
+                    6,
+                )
+                .unwrap();
+                (amounts.x, amounts.y)
+            }
+        };
+
+        require!(x <= max_x && y <= max_y, AmmError::SomeError);
+
+        Ok(())
+    }
+
+    fn deposit_token(&mut self, is_x: bool, amount: u64) -> Result<()> {
+        let (from, to) = match is_x {
+            true => (
+                self.lp_provider_mint_x_ata.to_account_info(),
+                self.vault_x.to_account_info(),
+            ),
+            false => (
+                self.lp_provider_mint_y_ata.to_account_info(),
+                self.vault_y.to_account_info(),
+            ),
+        };
+
+        let cpi_program = self.token_program.to_account_info();
+        let cpi_account = Transfer {
+            from, // lp_provider_mint_x_ata or lp_provider_mint_y_ata
+            to,   // vault_x or vault_y
+            authority: self.lp_provider.to_account_info(),
+        };
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_account);
+        transfer(cpi_ctx, amount)?;
+
+        Ok(())
+    }
+
+    fn mint_lp_tokens(&mut self, amount: u64) -> Result<()> {
+        let cpi_program = self.token_program.to_account_info();
+        let cpi_accounts = MintTo {
+            mint: self.mint_lp.to_account_info(),
+            to: self.lp_provider_mint_lp_ata.to_account_info(),
+            authority: self.config.to_account_info(),
+        };
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+        mint_to(cpi_ctx, amount)?;
+
+        let signer_seeds = &[
+            &b"config"[..],
+            &self.config.seed.to_le_bytes(),
+            &[self.config.config_bump],
+        ];
+        let signer_seeds = &[&seeds[..]];
+
+        let ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, signer_seeds);
+
+        mint_to(ctx, amount)?;
+        Ok(())
+    }
 }
--- programs/amm/src/lib.rs
+++ programs/amm/src/lib.rs
@@ -1,7 +1,10 @@
 use anchor_lang::prelude::*;
 
-pub mod state;
+pub mod error;
 pub mod instructions;
+pub mod state;
+
+use instructions::*;
 
 declare_id!("D6FyucvDAm3LxvqYzfncuqHM3hSU3wvZWEQkjw3htXKf");
 
@@ -9,11 +12,24 @@ declare_id!("D6FyucvDAm3LxvqYzfncuqHM3hSU3wvZWEQkjw3htXKf");
 pub mod amm {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
+    pub fn initialize(
+        ctx: Context<Initialize>,
+        seed: u64,
+        fee: u64,
+        authority: Option<Pubkey>,
+    ) -> Result<()> {
+        ctx.accounts.init(seed, fee, authority, &ctx.bumps)
+    }
+
+    pub fn deposit(ctx: Context<Deposit>, lp_amount: u64, max_x: u64, max_y: u64) -> Result<()> {
+        ctx.accounts.deposit(lp_amount, max_x, max_y)
+    }
+
+    pub fn withdraw(ctx: Context<Withdraw>, lp_amount: u64, min_x: u64, min_y: u64) -> Result<()> {
+        ctx.accounts.withdraw(lp_amount, min_x, min_y)
+    }
+
+    pub fn swap(ctx: Context<Swap>, is_x: bool, amount_in: u64, min_amount_out: u64) -> Result<()> {
+        ctx.accounts.swap(is_x, amount_in, min_amount_out)
     }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
